00c35cb5a8e1618dff264fcb7b4491e1703d2d2c
LucHighwalker <luc.highwalker@gmail.com>
2019-04-27 10:32:35 -0700

    - logging - added environments to dockerignore

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ dotenv.config();
 
 import server from "./server";
 
+const Sentry = require("@sentry/node");
+Sentry.init({
+  dsn: "https://df7a4422de87491094a011dd972ee11b@sentry.io/1445170"
+});
+
 const port = process.env.PORT || 4200;
 
 server.listen(port, (error: Error) => {
